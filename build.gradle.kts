33ad448db5ef7826655dc0f53a7936404a1accf3
Asoji <asoji-git@hiitsdevin.mozmail.com>
2024-09-09 07:57:41 -0700

    ⬆️ update yiski6

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,10 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.serialization") version "1.7.20"
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.ktor.plugin") version "2.3.12"
 }
 
 group = "one.devos"
@@ -24,7 +25,7 @@ val exposedVersion: String by project
 val ktorVersion: String by project
 
 dependencies {// Logger libraries for writing to the console
-    api("org.slf4j:slf4j-api:2.0.5")
+    api("org.slf4j:slf4j-api:2.0.13")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("ch.qos.logback:logback-core:$logbackVersion")
     // Kotlin
@@ -33,7 +34,7 @@ dependencies {// Logger libraries for writing to the console
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:$coroutinesVersion")
     // JSON Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.2")
     // TOML Serialization - YW Devin
     implementation("com.akuleshov7:ktoml-core:$ktomlVersion")
     implementation("com.akuleshov7:ktoml-file:$ktomlVersion")
@@ -51,23 +52,23 @@ dependencies {// Logger libraries for writing to the console
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-cio:$ktorVersion")
     // Aviation - Storm's command library made by Artrinix, will probably break
-    implementation("xyz.artrinix:aviation:8804d641")
+    implementation("xyz.artrinix:aviation:7d1dcef7")
     // Audio dependencies
     //implementation("dev.schlaubi.lavakord:jda:3.7.0")
     implementation("com.github.devoxin:lavaplayer:1.4.2")
     //implementation("com.sedmelluq:lavaplayer-natives:1.3.14")
     implementation("com.github.aikaterna:lavaplayer-natives:original-SNAPSHOT")
     // JDA
-    implementation("net.dv8tion:JDA:5.0.0-beta.6") {
+    implementation("net.dv8tion:JDA:5.0.1") {
         exclude(module = "opus-java")
     }
     // Kotlin Extensions for JDA
-    implementation("com.github.minndevelopment:jda-ktx:9fc90f6")
+    implementation("com.github.minndevelopment:jda-ktx:0.12.0")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
         freeCompilerArgs = listOf("-Xinline-classes", "-Xopt-in=kotlin.RequiresOptIn")
     }
 }
@@ -82,4 +83,11 @@ tasks.withType<ShadowJar> {
             )
         )
     }
+}
+
+application { // apparently needed for ktor? ok.
+    mainClass.set("one.devos.yiski.YiskiKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
